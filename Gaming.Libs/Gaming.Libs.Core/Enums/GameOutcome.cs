b9619f58e709af307cf06de5b1220f320119265e
jlafond <j.lafond55@gmail.com>
2021-07-22 20:10:27 -0400

    create core project, player object, game outcome enum

---
+++ Gaming.Libs/Gaming.Libs.Core/Enums/GameOutcome.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gaming.Libs.Core.Enums
+{
+    public enum GameOutcome
+    {
+        Win = 1,
+        Lose = 2,
+        Draw = 3
+    }
+}
---
+++ Gaming.Libs/Gaming.Libs.Core/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gaming.Libs.Core
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public int Wins { get; set; }
+        public int Lose { get; set; }
+        public int Draw { get; set; }
+    }
+}
