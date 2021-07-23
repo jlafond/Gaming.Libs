eb2b32eabb353894235cf9ba8533e80717ff6336
jlafond <j.lafond55@gmail.com>
2021-07-22 20:17:44 -0400

    Add comments, add methods to player class

--- Gaming.Libs/Gaming.Libs.Core/Enums/GameOutcome.cs
+++ Gaming.Libs/Gaming.Libs.Core/Enums/GameOutcome.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Gaming.Libs.Core.Enums
 {
+    /// <summary>
+    /// Used to determine if resulting outcome of the game is win, lose, or draw
+    /// </summary>
     public enum GameOutcome
     {
         Win = 1,
--- Gaming.Libs/Gaming.Libs.Core/Player.cs
+++ Gaming.Libs/Gaming.Libs.Core/Player.cs
@@ -7,11 +7,36 @@ namespace Gaming.Libs.Core
         public Player(string name)
         {
             Name = name;
+            Wins = 0;
+            Losses = 0;
+            Draws = 0;
         }
 
-        public string Name { get; set; }
-        public int Wins { get; set; }
-        public int Lose { get; set; }
-        public int Draw { get; set; }
+        public string Name { get; private set; }
+        private int Wins { get; set; }
+        private int Losses { get; set; }
+        private int Draws { get; set; }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+
+        public void AddWin()
+        {
+            Wins += 1;
+        }
+        public void AddLoss()
+        {
+            Losses += 1;
+        }
+        public void AddDraw()
+        {
+            Draws += 1;
+        }
+        public string Record()
+        {
+            return $"{Wins}-{Losses}-{Draws}";
+        }
     }
 }
